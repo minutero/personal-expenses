c9ab9e2e5502c7a26de964929a136bdb5e9a6fb0
Felipe Silva <felipe.silva.gon@gmail.com>
2021-12-30 00:49:52 -0300

    Title: Create function to read all TC files in folder
    Body: All data processing is now in a function so it can be iterated in all files to get the full history of TC information

--- read-dat.py
+++ read-dat.py
@@ -19,13 +19,13 @@ df = df.fillna(0)
 def cat_df(df):
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.startswith('TEF'),
                             'Transferencia','')
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('PAGO DE SERVICIOS|SERVICIOS|LIPIGAS|CHILQUI|PAC SERVICIOS PROFESIONA 20630|53324082-8|65146277-0|MUNIC.VILLA ALEMA|Felipe Itau|UNICEF|YOUTUBE'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('PAGO DE SERVICIOS|SERVICIOS|LIPIGAS|CHILQUI|PAC SERVICIOS PROFESIONA 20630|53324082-8|65146277-0|MUNIC.VILLA ALEMA|Felipe Itau|UNICEF|YOUTUBE|HBOMAX'),
                             'Cuentas',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('TARJ.CRED.'),
                             'Tarjeta Credito',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('SEGURO|SEG.|COMISION|MANTENCION|COM. MENSUAL|PAT VIDA SECURITY'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('SEGURO|SEG.|COMISION|MANTENCION|COM. MENSUAL|PAT VIDA SECURITY|IMPUESTO'),
                             'Financiero',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('EXPRESS|LIDER|HIPER|JUMBO|PANADERIA|ISABEL|MUNDO VERDE|ANIMAL|DISTRIBUIDORA AUS|12880128-6|12603011-8'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('EXPRESS|LIDER|HIPER|JUMBO|PANADERIA|ISABEL|MUNDO VERDE|ANIMAL|DISTRIBUIDORA AUS|12880128-6|12603011-8|TOTTUS'),
                             'Supermercado',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('MERVAL|PETROBRAS|SHELL|COPEC|ESTACIONAMIEN'),
                             'Transporte',df.Categoria)
@@ -41,9 +41,9 @@ def cat_df(df):
                             'SUMUP',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('CEMIN|SALCOBRAND|FARM|DR SIMI|AHUM'),
                             'Salud',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('PARIS|RIPLEY|CASAIDEAS|MODELPRO|76554187-5|76795499-9|77288155-K|JUEGO DE LETRAS|GATO ARCANO|MERCADO PAGO|EL CONTAINER|CENCOSUD'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('PARIS|RIPLEY|CASAIDEAS|MODELPRO|76554187-5|76795499-9|77288155-K|JUEGO DE LETRAS|GATO ARCANO|MERCADO PAGO|EL CONTAINER|CENCOSUD|AMAZON|O 2 SPORT|DAFITI|FPAY|LINIO'),
                             'Varios',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('MUNDO TRANSFER') ,
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('MUNDO TRANSFER|NUSKIN') ,
                             'Arigato',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Categoria == '' ,
                             'Indefinido',df.Categoria)
@@ -74,30 +74,45 @@ analyzed_df[col + ['Cargos']].nlargest(5,'Cargos')
 analyzed_df[col + ['Abonos']].nlargest(5,'Abonos')
 
 
+#### TC File
+def clean_files(dir):
+    df = pd.DataFrame()
+    files = []
+    for file in os.listdir(dir):
+        print(file)
+        df_tc = pd.read_excel(os.path.join(dir,file),skiprows=49)
 
-df_tc = pd.read_excel(os.path.join(directory,'EECC','Estado-de-Cuenta-Scotiabank-Diciembre-2021.xls'),skiprows=49)
+        #Remove unnecesary rows and columns
+        df_tc = df_tc[(df_tc['Fecha\nOperación'].notna())]
+        df_tc = df_tc.dropna(axis=1, how='all') #drop all empty columns
+        df_tc.columns = ['Lugar', 'Fecha', 'Codigo Referencia',
+            'Descripcion', 'Monto Operacion',
+            'Monto Total', 'N Cuota', 'Valor Cuota']
+        df_tc = df_tc[(df_tc['N Cuota'] != 'Cargo del Mes') & (df_tc['Descripcion'].str.strip() != 'PAGO')]
+        df_tc = df_tc[~df_tc['Descripcion'].str.strip().str.startswith('TOTAL')].reset_index(drop=True)
 
-#Remove unnecesary rows and columns
-df_tc = df_tc[(df_tc['Fecha\nOperación'].notna())]
-df_tc = df_tc.dropna(axis=1, how='all') #drop all empty columns
-df_tc.columns = ['Lugar', 'Fecha', 'Codigo Referencia',
-       'Descripcion', 'Monto Operacion',
-       'Monto Total', 'N Cuota', 'Valor Cuota']
-df_tc = df_tc[(df_tc['N Cuota'] != 'Cargo del Mes') & (df_tc['Descripcion'] != 'PAGO')]
-df_tc = df_tc[~df_tc['Descripcion'].str.strip().str.startswith('TOTAL')].reset_index(drop=True)
+        cat_df(df_tc)
 
-#Fix column information - String columns cleansing
-df_tc.loc[:,'Lugar'] = df_tc.loc[:,'Lugar'].str.strip()
-df_tc.loc[:,'Lugar'] = df_tc.Lugar.str.replace('VILLA ALEMA','VILLA ALEMANA')
-df_tc.loc[:,'Descripcion'] = df_tc.apply(lambda x: x['Descripcion'][:x['Descripcion'].find('TASA')], axis=1).str.strip()
-remove = re.compile("|".join(df_tc.Lugar.unique().tolist()))
-df_tc.loc[:,'Descripcion'] = df_tc.Descripcion.str.replace(remove,'').str.strip()
+        #Fix column information - String columns cleansing
+        df_tc.loc[:,'Fecha'] = pd.to_datetime(df_tc.Fecha.str.strip(),format='%d/%m/%Y', dayfirst=True)
+        df_tc.loc[:,'Lugar'] = df_tc.loc[:,'Lugar'].str.strip()
+        df_tc.loc[:,'Lugar'] = df_tc.Lugar.str.replace('VILLA ALEMA','VILLA ALEMANA')
+        df_tc.loc[:,'Descripcion'] = df_tc.apply(lambda x: x['Descripcion'][:x['Descripcion'].find('TASA')], axis=1).str.strip()
+        remove = re.compile("|".join(df_tc.Lugar.unique().tolist()))
+        df_tc.loc[:,'Descripcion'] = df_tc.Descripcion.str.replace(remove,'').str.strip()
 
-#Fix column information - Money columns cleansing
-df_tc[df_tc.columns[[4,5,7]]] = df_tc[df_tc.columns[[4,5,7]]].replace('[\$.]', '', regex=True).astype(float)
+        #Fix column information - Money columns cleansing
+        df_tc[df_tc.columns[[4,5,7]]] = df_tc[df_tc.columns[[4,5,7]]].replace('[\$.]', '', regex=True).astype(float)
 
-cat_df(df_tc)
+        df = df.append(df_tc)
+    df = df.sort_values('Fecha').reset_index(drop=True)
+    return df
+
+df_tc = clean_files(os.path.join(directory,'EECC'))
 
 df_tc.groupby('Categoria').sum()
 df_tc[col + ['Valor Cuota']].nlargest(5,'Valor Cuota')
 df_tc.sort_values('Categoria')
+
+df_tc[df_tc.Categoria == 'Indefinido']
+
