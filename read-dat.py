06128cf6acb34a5de8e53a76020d998516f2e137
Felipe Silva <felipe.silva.gon@gmail.com>
2021-12-31 13:56:39 -0300

    Title: Add more clasification words, 1 more arg
    Body: New words to identify more expenses into categories. Running from command can take 1 more argument for showing TC or not.

--- read-dat.py
+++ read-dat.py
@@ -13,17 +13,19 @@ import plotly.express as px
 def cat_df(df):
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.startswith('TEF'),
                             'Transferencia','')
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('SUMUP'),
+                            'SUMUP',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('PAGO DE SERVICIOS|SERVICIOS|LIPIGAS|CHILQUI|PAC SERVICIOS PROFESIONA 20630|53324082-8|65146277-0|MUNIC.VILLA ALEMA|Felipe Itau|UNICEF|YOUTUBE|HBOMAX'),
                             'Cuentas',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('TARJ.CRED.'),
                             'Tarjeta Credito',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('SEGURO|SEG.|COMISION|MANTENCION|COM. MENSUAL|PAT VIDA SECURITY|IMPUESTO'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('SEGURO|SEG.|COMISION|MANTENCION|COM. MENSUAL|PAT VIDA SECURITY|IMPUESTO|INTERES'),
                             'Financiero',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('EXPRESS|LIDER|HIPER|JUMBO|PANADERIA|ISABEL|MUNDO VERDE|ANIMAL|DISTRIBUIDORA AUS|12880128-6|12603011-8|TOTTUS'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('EXPRESS|LIDER|HIPER|JUMBO|PANADERIA|ISABEL|MUNDO VERDE|ANIMAL|DISTRIBUIDORA AUS|12880128-6|TOTTUS|EMPORIO|LUIS SOTO URRA|Rosana serey|Geni ve|Carlos arand|Venta de fru'),
                             'Supermercado',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('MERVAL|PETROBRAS|SHELL|COPEC|ESTACIONAMIEN'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('MERVAL|PETROBRAS|SHELL|COPEC|ESTACIONAMIEN|LATAM|PETRO LEIVA'),
                             'Transporte',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('GRIDO|RESTAUR|CAFETERIA|GLASGOW|SIN CULPA|HOTEL|ZOO|ENTREFUEGO|CAFE|MISTER DI|KEYER|FUNDACION JARDIN'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('GRIDO|RESTAUR|CAFETERIA|GLASGOW|SIN CULPA|HOTEL|ZOO|ENTREFUEGO|CAFE|MISTER DI|KEYER|FUNDACION JARDIN|12603011-8|LA CASA DEL JARDI'),
                             'Salida',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('GIRO REDBANC'),
                             'Efectivo',df.Categoria)
@@ -31,19 +33,21 @@ def cat_df(df):
                             'Construccion',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('76810627-4|76989370-9|5335679-6|8671486-8'),
                             'Inversion',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('SUMUP'),
-                            'SUMUP',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('CEMIN|SALCOBRAND|FARM|DR SIMI|AHUM'),
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('CEMIN|SALCOBRAND|FARM|DR SIMI|AHUM|ISAPRE'),
                             'Salud',df.Categoria)
-    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('PARIS|RIPLEY|CASAIDEAS|MODELPRO|76554187-5|76795499-9|77288155-K|JUEGO DE LETRAS|GATO ARCANO|MERCADO PAGO|EL CONTAINER|CENCOSUD|AMAZON|O 2 SPORT|DAFITI|FPAY|LINIO'),
-                            'Varios',df.Categoria)
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('PARIS|RIPLEY|CASAIDEAS|MERCADO PAGO|EL CONTAINER|CENCOSUD|O 2 SPORT|DAFITI|FPAY|LINIO|FALABELL|MERCADOPAGO'),
+                            'Casa Comercial',df.Categoria)
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('77288155-K|JUEGO DE LETRAS|GATO ARCANO|MODELPRO|76554187-5|76795499-9|AMAZON|CAFE Y TABLEROS'),
+                            'Juegos',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('MUNDO TRANSFER|NUSKIN') ,
                             'Arigato',df.Categoria)
+    df.loc[:,'Categoria'] = np.where(df.Descripcion.str.contains('76574150-5') ,
+                            'Trabajo',df.Categoria)
     df.loc[:,'Categoria'] = np.where(df.Categoria == '' ,
                             'Indefinido',df.Categoria)
 
 def clean_main(full_path):
-    print('Cleaning TD expenses file ' + full_path)
+    print('\n' + 'Creating TD expenses file from: \n' + full_path + '\n')
     df = pd.read_csv(full_path, sep=";",skiprows=8,dtype={'Fecha': str} ,decimal=',')
     #Fecha
     df.loc[:,'Fecha'] = pd.to_datetime(df.Fecha.str.strip(),format='%d%m%Y', dayfirst=True)
@@ -58,13 +62,25 @@ def clean_main(full_path):
 def build_report(df):
     try:
         mes = sys.argv[1]
-    except:
+    except:        
         raise TypeError("missing 1 required argument: 'mes'. You need to specify a month like Dec2021 as an argument")
+    try:
+        tc = list(range(int(sys.argv[2])+1))
+    except:
+        tc = list(range(2))
 
-    analyzed_df = df[(df.Mes == mes) & (df.Categoria != 'Tarjeta Credito')]    
-    total_cat = pd.DataFrame(analyzed_df[['Categoria','Cargos']].groupby('Categoria').sum()).reset_index()    
+    analyzed_df = df[(df.Mes == mes) & (df.Categoria != 'Tarjeta Credito') & (df.TC.isin(tc))]
+    analyzed_df.loc[:,'TC'] = analyzed_df.TC.apply(np.int64).astype(str)
+    total_cat = pd.DataFrame(analyzed_df[['Categoria','Cargos']].groupby('Categoria').sum()).reset_index()
 
-    fig =px.bar(analyzed_df, x='Categoria', y='Cargos', hover_name="Descripcion", color='Categoria', title='Gastos para ' + mes + '. (Total: ' + "${:,.0f}".format(int(total_cat.Cargos.sum())) + ')')
+    fig =px.bar(analyzed_df, x='Categoria', y='Cargos',
+            hover_name="Descripcion",
+            hover_data = ["TC"],
+            color='TC',
+            title='Gastos para ' + mes + '. (Total: ' + "${:,.0f}".format(int(total_cat.Cargos.sum())) + ')',
+            color_discrete_map={"0": 'indianred',"1":'darksalmon'})
+    fig.update_xaxes(title='')
+    fig.update_yaxes(title='Monto')
 
     for i, t in total_cat.iterrows():        
         fig.add_annotation(x=t.Categoria ,y=t.Cargos+10000 ,text="${:,.0f}".format(t.Cargos),showarrow=False)
@@ -73,14 +89,14 @@ def build_report(df):
     '''
     analyzed_df[['Mes','Cargos','Categoria']].groupby(['Mes','Categoria']).max()
 
-    col = ['Fecha','Descripcion','Categoria']
-    analyzed_df[col + ['Cargos']].nlargest(5,'Cargos')
+    col = ['Fecha','Descripcion','Categoria','TC']
+    analyzed_df[col + ['Cargos']][~analyzed_df.Descripcion.str.contains('16332063-0|16261568-8')].nlargest(5,'Cargos')
     analyzed_df[col + ['Abonos']].nlargest(5,'Abonos')
     '''
 
 #### TC File
 def clean_files(dir):
-    print('Creating consolidate TC expenses file with the following files: \n')
+    print('Creating consolidate TC expenses file with the following files:')
     df = pd.DataFrame()
     files = []
     for file in os.listdir(dir):
@@ -123,11 +139,11 @@ def clean_files(dir):
     return df
 
 def main():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         directory = r'C:\Users\felip\Documents\Gastos'
         file = 'ScotiaFS2021.dat'
     else:
-        path = os.path.split(sys.argv[2])
+        path = os.path.split(sys.argv[3])
         directory = path[0]
         file = path[1]
     
