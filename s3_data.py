a23b125d6c8d45372e52457e4c2e04fc2258c493
minutero <felipe.silva.gon@gmail.com>
2022-04-06 13:05:05 -0400

    Title: Added write to S3 function
    Body: Function to upload certain outputs to S3

--- s3_data.py
+++ s3_data.py
@@ -13,3 +13,10 @@ def read_from_s3(file_key, s3_client=s3, bucket='fs-personal-expenses'):
     df = pd.read_csv(io.StringIO(csv_string))
     df.loc[:,'Fecha'] = pd.to_datetime(df.Fecha.str.strip(),format='%Y-%m-%d')
     return df
+
+def write_to_s3(df_to_upload,bucket,file_key):
+    csv_buffer = io.StringIO()
+    df_to_upload.to_csv(csv_buffer, index=False)
+
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object(bucket, file_key).put(Body=csv_buffer.getvalue())
